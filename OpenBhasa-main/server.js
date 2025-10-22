5fbc5381dc1ef932083a889d6bf672528f633982
Ashutosh-0812 <ashutoshsantoshsingh05@gmail.com>
2025-10-22 16:10:19 +0530

    backend deployment

--- OpenBhasa-main/server.js
+++ OpenBhasa-main/server.js
@@ -19,7 +19,8 @@ const allowedOrigins = [
   'https://openbhasa-frontend.onrender.com',
   'http://localhost:5173',
   'http://localhost:3000',
-  'http://localhost:5174'
+  'http://localhost:5174',
+  'http://127.0.0.1:5173'
 ].filter(Boolean); // Remove undefined values
 
 app.use(
